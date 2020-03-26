f35597d50fd9ebc76e592158be5fb001ae784b38
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2020-03-26 15:53:33 +0100

    Fix gradle build. Downgrade to scala 2.12.

--- mirror-home/build.gradle.kts
+++ mirror-home/build.gradle.kts
@@ -1,11 +1,11 @@
 group = "org.bmp"
-version = "1.0.0"
+version = "0.0.0"
 
 plugins {
     scala
     application
-    id("org.scoverage") version "4.0.1"
-    id("com.github.maiflai.scalatest") version "0.24"
+    id("org.scoverage") version "3.0.0"
+    id("com.github.maiflai.scalatest") version "0.25"
 }
 
 application {
@@ -18,15 +18,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.1")
-    //implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("com.typesafe.scala-logging:scala-logging_2.13:3.9.2")
+    implementation("org.scala-lang:scala-library:2.12.11")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.typesafe.scala-logging:scala-logging_2.12:3.9.2")
 
+    implementation("com.typesafe.akka:akka-http_2.12:10.1.11")
+    implementation("com.typesafe.akka:akka-stream_2.12:2.5.26")
     //compile("org.scala-lang:scala-library:2.13.1")
 
-    testImplementation("org.scalatest:scalatest_2.13:3.0.8")
+    testImplementation("org.scalatest:scalatest_2.12:3.0.1")
     testImplementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.0")
     testRuntimeOnly("org.pegdown:pegdown:1.4.2")
-    scoverage("org.scoverage:scalac-scoverage-plugin_2.13:1.4.0")
-    scoverage("org.scoverage:scalac-scoverage-runtime_2.13:1.4.0")
+    scoverage("org.scoverage:scalac-scoverage-plugin_2.12:1.3.1")
+    scoverage("org.scoverage:scalac-scoverage-runtime_2.12:1.3.1")
 }
