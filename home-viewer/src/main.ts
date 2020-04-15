ef60f1fc6342ce20182b69154e724beef330012d
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2020-04-15 19:49:45 +0200

    Add home representation in store.

--- home-viewer/src/main.ts
+++ home-viewer/src/main.ts
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import { getHome } from '@/Api'
+ 
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +11,13 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+function updateHomedata(interval: number) {
+  setInterval(() => {
+    getHome(home => {
+      store.commit("updateHome", home)
+    })
+  }, interval)
+}
+
+updateHomedata(1000);
